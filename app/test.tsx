401c1ee0fae265b150644719f2fee9dbed22633e
Parvelmarv <povel.croona@gmail.com>
2025-06-03 16:10:55 +0200

    updated rate limiting, but need to fix more

--- app/test.tsx
+++
@@ -1,7 +0,0 @@
-export default function Test() {
-  return (
-    <div className="min-h-screen bg-red-500 flex items-center justify-center">
-      <h1 className="text-4xl font-bold text-white">If you see this in red with white text, Tailwind is working!</h1>
-    </div>
-  );
-} 
